eae4d342a223e0d0124f7c0d144cfe90a73dbc90
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2020-09-17 12:44:43 +0300

    edit1

--- lab1variant4/main.c
+++ lab1variant4/main.c
@@ -6,10 +6,23 @@
 //  Copyright © 2020 gru gri. All rights reserved.
 //
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include<stdio.h>
+int main()
+{
+    int a, i=1;
+    printf("enter a: ");
+    scanf("%d", &a);
+    while(i < a){
+        i*=3;
+        
+            }
+    if (i==a){
+        printf ("true");
+        
+    }
+    else{
+        printf ("false");
+    }
+    return(0);
+    
 }
