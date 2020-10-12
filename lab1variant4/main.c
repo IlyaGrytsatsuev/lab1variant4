d9a38b0f8e2c3aa111c25aea531a20ad82c87178
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2020-10-12 20:35:54 +0300

    adiition

--- lab1variant4/main.c
+++ lab1variant4/main.c
@@ -9,20 +9,43 @@
 #include<stdio.h>
 int main()
 {
-    int a, i=1;
-    printf("enter the number: ");
+    int a, i=1,x = 0;
+    printf("enter the number in power: ");
     scanf("%d", &a);
-    while(i < a){
-        i*=3;
-        
+    printf("enter the number:  ");
+    scanf("%d", &x);
+    if (a == -1&&x == 1){
+        printf("false\n");
+        return 0;
+    }
+    if((a!=1)&&(x==1)){
+    printf("false\n");
+        return 0;
+    }
+    if((a!=1)&&(x==-1)){
+        printf("false\n");
+        return 0;
+    }
+    if(a > 0){
+        while(i<a){
+        i*=x;
             }
-    if (i==a){
-        printf ("RESULT: true\n");
-        
+    if (i==a||a==0||a==1) {
+        printf("RESULT: true\n");
+          }
+    else {
+        printf("false");
     }
-    else{
-        printf ("RESULT: false\n");
     }
-    return(0);
-    
+    else if (a < 0){
+        while(i>a){
+            i*=x;
+                }
+        if (i==a||a==0) {
+            printf("RESULT: true\n");
+              }
+        else {
+            printf("false\n");    }
 }
+}
+    
